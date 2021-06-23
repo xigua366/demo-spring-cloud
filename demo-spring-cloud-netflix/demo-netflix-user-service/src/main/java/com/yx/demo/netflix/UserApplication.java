77b2af0e445d44419b6a761dbeba7c970dee8ac9
yangxi <yangxi@deepexi.com>
2021-06-23 10:32:23 +0800

    统一调整依赖与工程结构

---
+++ demo-spring-cloud-netflix/demo-netflix-user-service/src/main/java/com/yx/demo/netflix/UserApplication.java
@@ -0,0 +1,21 @@
+package com.yx.demo.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author yangxi
+ * @version 1.0
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
